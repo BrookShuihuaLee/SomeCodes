b7b9cae4ae0e400490705778eba41fb2cfca13d0
Shuihua Li <lishuihua@rd.netease.com>
2018-12-15 19:44:59 +0800

    mod: calculator test

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -7,4 +7,5 @@ test('Calculator test', () => {
     expect(calculator.run('3+5*(2-1)')).toBe(8)
     expect(calculator.run('3+5*(2-1)/(10-3-2)-4/1')).toBe(0)
     expect(calculator.run('3+5*(2-1)/(10-3-2)-4/1-4*4*4')).toBe(-64)
+    expect(calculator.run('8/2')).toBe(4)
 })
