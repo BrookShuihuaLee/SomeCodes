c5f00568f67a41a6fd94266f9be45b84eb47a82a
Shuihua Li <lishuihua@rd.netease.com>
2018-12-16 10:29:37 +0800

    mod: calculator

--- src/calculator.js
+++ src/calculator.js
@@ -7,60 +7,60 @@ const OP_MAP = {
 
 export default class Calculator {
     _tokens = null
-
-    run (input) {
+    
+    run(input) {
         this._tokenize(input)
         return this._expression()
     }
 
-    _tokenize (input) {
+    _tokenize(input) {
         this._tokens = (
             input
-                .match(/\d+\.\d+|\d+|[\+\-\*\/\(\)]/g)
+                .match(/\d+(\.\d+)?|[\+\-\*\/\(\)]/g)
                 .reverse()
         )
     }
 
-    _peekToken () {
-        return this._tokens[this._tokens.length - 1]
-    }
-
-    _popToken () {
+    _popToken() {
         return this._tokens.pop()
     }
 
-    _accept (s) {
-        return this._peekToken() === s && this._popToken()
+    _peekToken() {
+        return this._tokens[this._tokens.length - 1]
     }
 
-    _acceptNumber () {
+    _accept(t) {
+        return t === this._peekToken() && this._popToken()
+    }
+
+    _acceptAny(ts) {
+        return ts.some(t => t === this._peekToken()) && this._popToken()
+    }
+
+    _acceptNumber() {
         return !isNaN(this._peekToken()) && this._popToken()
     }
 
-    _acceptAny (a) {
-        return a.some(s => this._peekToken() === s) && this._popToken()
-    }
-
-    _doOp (op, a, b) {
+    _doOp(op, a, b) {
         return OP_MAP[op](a, b)
     }
 
-    _unit () {
+    _unit() {
         let v
 
         if (this._accept('(')) {
             v = this._expression()
             this._accept(')')
         } else v = Number(this._acceptNumber())
-        
+
         return v
     }
 
-    _unary () {
-        return this._accept('-') ? -this._unit() : this._unit()
+    _unary() {
+        return (this._accept('-') ? -1 : 1) * this._unit()
     }
 
-    _factor () {
+    _factor() {
         let v = this._unary()
         let op
 
@@ -69,7 +69,7 @@ export default class Calculator {
         return v
     }
 
-    _expression () {
+    _expression() {
         let v = this._factor()
         let op
 
